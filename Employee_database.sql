3e9989218efaa4e11320f9f97bfca246aa5d142a
Deekshitha-reddy624 <reddyyvangadekshitha@gmail.com>
2026-06-30 18:24:41 +0530

    Create Employees table and insert initial data

---
+++ Employee_database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT
+);
+
+INSERT INTO Employees VALUES (1, 'John', 'IT', 30000);
+INSERT INTO Employees VALUES (2, 'Sarah', 'HR', 35000);
+INSERT INTO Employees VALUES (3, 'Ali', 'Sales', 40000);
